5a82ee2a5453ad25c9807f835e34764378154a98
Xabier Lazcoz <ikxlazcoz25@lhusurbil.eus>
2025-12-16 14:08:52 +0100

    produktuak

---
+++ javascript.js
@@ -0,0 +1,306 @@
+const produktuak = [
+  // --- CAMISETAS ---
+  {
+    productName: "Camiseta blanca",
+    price: 15,
+    img: "img/camiseta blanca.png",
+    category: 'camisetas'
+  },
+  {
+    productName: "Camiseta negra",
+    price: 18,
+    img: "img/camiseta negra.png",
+    category: 'camisetas'
+  },
+  {
+    productName: "Camiseta gris",
+    price: 20,
+    img: "img/camiseta gris.png",
+    category: 'camisetas'
+  },
+  {
+    productName: "camiseta azul oscuro",
+    price: 22,
+    img: "img/camiseta azul oscuro.png",
+    category: 'camisetas'
+  },
+  {
+    productName: "camiseta beige",
+    price: 15,
+    img: "img/camiseta beige.png",
+    category: 'camisetas'
+  },
+  {
+    productName: "Camiseta de rayas granate",
+    price: 25,
+    img: "img/Camiseta de rayas granate.png",
+    category: 'camisetas'
+  },
+  {
+    productName: "Camiseta azul claro",
+    price: 30,
+    img: "img/Camiseta azul claro.png",
+    category: 'camisetas'
+  },
+  {
+    productName: "camiseta verde pastel",
+    price: 12,
+    img: "img/camiseta verde pastel.png",
+    category: 'camisetas'
+  },
+  {
+    productName: "camiseta amarilla",
+    price: 18,
+    img: "img/camiseta amarilla.png",
+    category: 'camisetas'
+  },
+  {
+    productName: "camiseta verde",
+    price: 20,
+    img: "img/camiseta verde.png",
+    category: 'camisetas'
+  },
+
+  // --- PANTALONES ---
+  {
+    productName: "Pantalón vaquero",
+    price: 40,
+    img: "img/pantalon vaquero.png",
+    category: 'pantalones'
+  },
+  {
+    productName: "Pantalón beige",
+    price: 45,
+    img: "img/pantalon beige.png",
+    category: 'pantalones'
+  },
+  {
+    productName: "Pantalón chino negro",
+    price: 35,
+    img: "img/pantalon chino negro.png",
+    category: 'pantalones'
+  },
+  {
+    productName: "Pantalón cargo verde",
+    price: 50,
+    img: "img/pantalon cargo verde.png",
+    category: 'pantalones'
+  },
+  {
+    productName: "Pantalón vaquero oversize",
+    price: 55,
+    img: "img/Pantalon vaquero oversize.png",
+    category: 'pantalones'
+  },
+  {
+    productName: "Pantalón de pana naranja",
+    price: 40,
+    img: "img/Pantalon de pana naranja.png",
+    category: 'pantalones'
+  },
+  {
+    productName: "Pantalón chino gris con patron",
+    price: 60,
+    img: "img/Pantalon chino gris con patron.png",
+    category: 'pantalones'
+  },
+  {
+    productName: "Pantalón chino verde",
+    price: 30,
+    img: "img/Pantalon chino verde.png",
+    category: 'pantalones'
+  },
+  {
+    productName: "Pantalón de chandal gris",
+    price: 45,
+    img: "img/Pantalon de chandal gris.png",
+    category: 'pantalones'
+  },
+  {
+    productName: "Pantalón de cuero",
+    price: 70,
+    img: "img/pantalones de cuero.png",
+    category: 'pantalones'
+  },
+
+  // --- CHAQUETAS ---
+  {
+    productName: "Chaqueta vaquera",
+    price: 60,
+    img: "img/chaqueta vaquera.png",
+    category: 'chaquetas'
+  },
+  {
+    productName: "Chaqueta Bomber Negra",
+    price: 80,
+    img: "img/Chaqueta Bomber Negra.png",
+    category: 'chaquetas'
+  },
+  {
+    productName: "Gabardina Beige con Cinturón",
+    price: 75,
+    img: "img/Gabardina Beige con Cinturón.png",
+    category: 'chaquetas'
+  },
+  {
+    productName: "Chaqueta de Punto Gris",
+    price: 90,
+    img: "img/Chaqueta de Punto Gris.png",
+    category: 'chaquetas'
+  },
+  {
+    productName: "Chaqueta Estilo Militar",
+    price: 65,
+    img: "img/Chaqueta Estilo Militar.png",
+    category: 'chaquetas'
+  },
+  {
+    productName: "Impermeable amarillo con Capucha ",
+    price: 100,
+    img: "img/Impermeable amarillo con Capucha.png",
+    category: 'chaquetas'
+  },
+  {
+    productName: "Chaqueta de Cuero Marrón",
+    price: 85,
+    img: "img/Chaqueta de Cuero Marrón.png",
+    category: 'chaquetas'
+  },
+  {
+    productName: "Abrigo Cruzado Azul Marino",
+    price: 70,
+    img: "img/Abrigo Cruzado Azul Marino.png",
+    category: 'chaquetas'
+  },
+  {
+    productName: "Cardigan de Punto Beige",
+    price: 95,
+    img: "img/Cardigan de Punto Beige.png",
+    category: 'chaquetas'
+  },
+  {
+    productName: "Chaqueta de Plumas Verde Oliva",
+    price: 110,
+    img: "img/Chaqueta de Plumas Verde Oliva.png",
+    category: 'chaquetas'
+  },
+
+  // --- ZAPATILLAS ---
+  {
+    productName: "Zapatillas deportivas blancas",
+    price: 50,
+    img: "img/Zapatillas deportivas blancas.png",
+    category: 'zapatillas'
+  },
+  {
+    productName: "Puma Urban Black",
+    price: 60,
+    img: "img/Puma Urban Black.png",
+    category: 'zapatillas'
+  },
+  {
+    productName: "Converse All Star Grey",
+    price: 75,
+    img: "img/Converse All Star Grey.png",
+    category: 'zapatillas'
+  },
+  {
+    productName: "Vans Authentic Beige",
+    price: 80,
+    img: "img/Vans Authentic Beige.png",
+    category: 'zapatillas'
+  },
+  {
+    productName: "Nike Air Sport Multicolor",
+    price: 55,
+    img: "img/Nike Air Sport Multicolor.png",
+    category: 'zapatillas'
+  },
+  {
+    productName: "Adidas Yung Cream",
+    price: 90,
+    img: "img/Adidas Yung Cream.png",
+    category: 'zapatillas'
+  },
+  {
+    productName: "Vans Authentic Coral",
+    price: 65,
+    img: "img/Vans Authentic Coral.png",
+    category: 'zapatillas'
+  },
+  {
+    productName: "Vans Authentic Coral rosa",
+    price: 70,
+    img: "img/Vans Authentic Coral rosa.png",
+    category: 'zapatillas'
+  },
+  {
+    productName: "Vans Suede Green",
+    price: 85,
+    img: "img/Vans Suede Green.png",
+    category: 'zapatillas'
+  },
+  {
+    productName: "Nike Air Sport rojo",
+    price: 95,
+    img: "img/Nike Air Sport rojo.png",
+    category: 'zapatillas'
+  }
+];
+
+function erakutsiProduktuak(erakustekoProduktuak) {
+  const produktuenZerrenda = document.getElementById("produktuenZerrenda")
+  
+  produktuenZerrenda.innerHTML = ""
+  
+  erakustekoProduktuak.forEach(produktua => {
+    const div = document.createElement("div")
+    div.className = 'produktu-txartela'
+    
+    div.innerHTML = `
+      <img src="${produktua.img}" alt="${produktua.productName}">
+      <h3>${produktua.productName}</h3>
+      <p class="prezioa">$ ${produktua.price}</p>
+      <button>Gehitu saskira</button>
+    `
+    
+    produktuenZerrenda.append(div)
+  })
+}
+
+function iragaziProduktuak(kategoria) {
+  const erakustekoProduktuak = produktuak.filter(produktua => {
+    return produktua.category === kategoria
+  })
+  
+  erakutsiProduktuak(erakustekoProduktuak)
+}
+
+
+const btnCamisetas = document.getElementById('camisetas');
+const btnPantalones = document.getElementById('pantalones');
+const btnZapatillas = document.getElementById('zapatillas');
+const btnChaquetas = document.getElementById('chaquetas');
+const btnTodos = document.getElementById('produktuak');
+
+btnCamisetas.addEventListener('click', function() {
+  iragaziProduktuak('camisetas');
+});
+
+btnPantalones.addEventListener('click', function() {
+  iragaziProduktuak('pantalones');
+});
+
+btnZapatillas.addEventListener('click', function() {
+  iragaziProduktuak('zapatillas');
+});
+
+btnChaquetas.addEventListener('click', function() {
+  iragaziProduktuak('chaquetas');
+});
+
+btnTodos.addEventListener('click', function() {
+  erakutsiProduktuak(produktuak)
+});
+
+erakutsiProduktuak(produktuak)
