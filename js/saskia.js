0c459fb8f16405e06743f860f4945b15281c8aec
ikjortiz25 <ikjortiz25@example.com>
2026-01-14 09:33:03 +0100

    azken aldaketak

---
+++ js/saskia.js
@@ -0,0 +1,8 @@
+function irekiDialogoa() {
+  document.getElementById("popup").style.display = "block";
+}
+
+
+function itxiDialogoa() {
+  document.getElementById("popup").style.display = "none";
+}
